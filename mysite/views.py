d7afed4affb134b8860464b1f9ce96012945c3d3
ikea1229 <11056019@ntub.edu.tw>
2023-10-28 21:58:25 +0800

    goood

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from mysite.models import Post
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import redirect
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request,'index.html',locals())
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            return render(request,'post.html',locals())
+        else:
+            return redirect("/")
+    except:
+        return redirect("/")
+    
+'''
+def homepage(request):
+    posts = Post.objects.all()   #select*from post
+    post_lists = list()
+    for counter, post in enumerate(posts):
+        post_lists.append(f'No.{counter}-{post} <br>')  #<br>網頁換行
+    return HttpResponse(post_lists)
+'''
